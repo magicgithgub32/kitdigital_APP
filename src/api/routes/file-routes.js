f4953cd9ada25877b4e971c31db23dff0701658b
magicgithgub32 <rubcastellon@gmail.com>
2024-01-05 08:52:15 +0100

    create controller and route for requestProcessBono

--- src/api/routes/file-routes.js
+++ src/api/routes/file-routes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { processKitDigitalFile } = require("../controllers/file-controllers");
+const {
+  processKitDigitalFile,
+  processRequestBono,
+} = require("../controllers/file-controllers");
 const uploadExcelFile = require("../middlewares/uploadExcelFile-middleware");
 
 router.post(
   "/upload",
   uploadExcelFile.single("excelFile"),
   processKitDigitalFile
+);
+
+router.post(
+  "/requestBono",
+  uploadExcelFile.single("excelFile"),
+  processRequestBono
 );
 
 module.exports = router;
